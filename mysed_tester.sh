6154907cbbea39b097e57967defaff281d2570c8
alterGNU <alterGNU@gmail.com>
2026-02-08 11:40:23 +0100

    ADD: main handle arguments and set binary path

--- mysed_tester.sh
+++ mysed_tester.sh
@@ -2,7 +2,7 @@
 
 # ============================================================================================================
 # Launch mysed tests
-# - Usage: ./mysed_tester.sh binary_path "match_pattern" "replace_pattern"
+# - Usage: ./mysed_tester.sh binary_filename "match_pattern" "replace_pattern"
 # ============================================================================================================
  
 # =[ VARIABLES ]==============================================================================================
@@ -11,8 +11,8 @@ ARGS=()                                                           # ☒ Copy of 
 for arg in "${@}";do ARGS+=( "${arg}" );done
 NB_ARG="${#}"                                                     # ☒ Number of script's arguments
 SCRIPTNAME=${0##*\/}                                              # ☒ Script's name (no path)
-PARENT_DIR=$(cd $(dirname ${0}) && pwd)                           # ☒ Name of parent directory (TEST_DIR)
-MS_DIR=$(cd $(dirname ${PARENT_DIR}) && pwd)                      # ☒ Name of great-parent directory (CUB3D_DIR)
+PARENT_DIR=$(cd $(dirname ${0}) && pwd)                           # ☒ Name of parent directory
+GP_DIR=$(cd $(dirname ${PARENT_DIR}) && pwd)                      # ☒ Name of great-parent directory
 # -[ LAYOUT ]-------------------------------------------------------------------------------------------------
 LEN=100                                                            # ☑ Width of the box
 # -[ COLORS ]-------------------------------------------------------------------------------------------------
@@ -41,6 +41,24 @@ print_shorter_path() { echo "${1/${PWD}/.}" ; }
 # ============================================================================================================
 # =[ Handle Script Arguments ]================================================================================
 if [ ${NB_ARG} -ne 3 ];then
-	echo -e "${R0}Usage: ./${SCRIPTNAME} <binary_path> <match_pattern> <replace_pattern>${E}"
+	echo -e "${RU}Error:${R0} this script requires exactly 3 arguments${E}"
+	echo -e "${BU}Usage:${E} ${V0}./${SCRIPTNAME}${E} <${M0}binary_filename${E}> <${M0}match_pattern${E}> <${M0}replace_pattern${E}>"
 	exit 1
 fi
+# =[ Assign arguments to variables ]==========================================================================
+[[ -z "${ARGS[0]}" ]] && { echo -e "${RU}Error:${R0} first script argument is binary_filename and can not be empty${E}" && exit 1; }
+BIN_PATH=$(find ${GP_DIR} -type f -name "${ARGS[0]}" 2>/dev/null)
+if [[ -z "${BIN_PATH}" ]]; then
+	echo -e "${RU}Error:${R0} binary file '${Y0}${ARGS[0]}${R0}' not found in ${M0}${GP_DIR}${E}"
+	exit 1
+else
+	if [[ ! -x "${BIN_PATH}" ]]; then
+		echo -e "${RU}Error:${R0} file ${Y0}$(print_shorter_path ${BIN_PATH})${R0} is not executable${E}"
+		exit 1
+	else
+		echo -e "${B0} * ${BU}Binary file path set at:${E} ${G0}${BIN_PATH}${E}"
+	fi
+fi
+# =[ Build folder and files ]=================================================================================
+
+# =[ Run tests ]==============================================================================================
